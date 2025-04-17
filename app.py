d5e437b190a26d6690357d2ada84357b6c6f3857
Karnika Deore <karnikadeore031104@gmail.com>
2025-04-17 23:36:56 +0530

    saved code

---
+++ app.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, request, redirect
+from pymysql import connections
+import MySQLdb
+import boto3
+import os
+from config import *
+
+app = Flask(__name__)
+
+bucket = custombucket
+region = customregion
+
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+s3 = boto3.client('s3')
+
+db_conn = connections.Connection(
+    host=customhost,
+    port=3306,
+    user=customuser,
+    password=custompass,
+    db=customdb
+)
+output = {}
+table = 'employee'
+
+
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add_employee', methods=['POST'])
+def add_employee():
+    empid = request.form['empid']
+    fname = request.form['fname']
+    lname = request.form['lname']
+    pri_skill = request.form['pri_skill']
+    location = request.form['location']
+    image = request.files['image']
+
+    if image:
+        img_filename = f"{empid}_{image.filename}"
+        image.save(os.path.join(app.config['UPLOAD_FOLDER'], img_filename))
+        s3.upload_file(os.path.join(app.config['UPLOAD_FOLDER'], img_filename), custombucket, img_filename)
+
+    cursor = db_conn.cursor()
+    cursor.execute("INSERT INTO employee (empid, fname, lname, pri_skill, location) VALUES (%s, %s, %s, %s, %s)",
+                   (empid, fname, lname, pri_skill, location))
+    db_conn.commit()
+    cursor.close()
+
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,9 @@
+customhost = "employee.cngwgohslfp3.us-east-1.rds.amazonaws.com"
+customuser = "karnika"
+custompass = "KarnikaD"  # Add your actual DB password here
+customdb = "employee"
+custombucket = "addemployeekd"
+customregion = "us-east-1"
+
+# Optional: S3 URL (if you're displaying uploaded images)
+S3_LOCATION = f"https://{custombucket}.s3.{customregion}.amazonaws.com/"
